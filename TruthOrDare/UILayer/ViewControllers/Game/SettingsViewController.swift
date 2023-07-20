b21cdb790983496d8394d5907170bf5938677327
Imran Hajiyev <haciyev008@gmail.com>
2023-07-20 18:50:18 +0400

    feature: added settings screen initial page design

--- TruthOrDare/UILayer/ViewControllers/Game/SettingsViewController.swift
+++ TruthOrDare/UILayer/ViewControllers/Game/SettingsViewController.swift
@@ -9,23 +9,46 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    @IBOutlet weak var gameModeStackView: UIStackView!
+    @IBOutlet weak var chatGPTStackView: UIStackView!
+    @IBOutlet weak var customStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.addBorder(to: [gameModeStackView, chatGPTStackView, customStackView])
+
+        let gameModeTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+        let gameModeLongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(tap2))
+        self.gameModeStackView.addGestureRecognizer(gameModeTapGestureRecognizer)
+        self.gameModeStackView.addGestureRecognizer(gameModeLongPressGestureRecognizer)
     }
 
+    @objc func tap() {
+        print("tapped")
+    }
+    
+    @objc func tap2(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        switch gestureRecognizer.state {
+        case .began:
+            self.gameModeStackView.backgroundColor = .systemGray6
+        case .ended:
+            self.gameModeStackView.backgroundColor = .white
+        default:
+            break
+        }
+    }
+    
     @IBAction func onBack(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func addBorder(to stackViews: [UIStackView]) {
+        for stackView in stackViews {
+            stackView.layer.borderWidth = 1
+            stackView.layer.borderColor = UIColor.black.cgColor
+            stackView.layer.cornerRadius = 6
+        }
     }
-    */
-
 }
