2a2f50a80ddeb90ad29cbb584d1b4f350ae3d688
Imran Hajiyev <haciyev008@gmail.com>
2023-08-09 17:40:10 +0400

    feature: text view configured for add custom content screen

--- TruthOrDare/UILayer/ViewControllers/Settings/Content/AddCustomContentViewController.swift
+++ TruthOrDare/UILayer/ViewControllers/Settings/Content/AddCustomContentViewController.swift
@@ -14,6 +14,7 @@ class AddCustomContentViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var contentTextViewBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +30,8 @@ class AddCustomContentViewController: UIViewController, UITextViewDelegate {
         // Add tap gesture recognizer to listen to external taps.
         let keyboardOutsideTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         self.view.addGestureRecognizer(keyboardOutsideTapGestureRecognizer)
+        
+        self.configureKeyboardNotifications()
     }
 
     @IBAction func onCancel(_ sender: Any) {
@@ -49,7 +52,63 @@ class AddCustomContentViewController: UIViewController, UITextViewDelegate {
         }
     }
     
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
     @objc func dismissKeyboard() {
         self.view.endEditing(true)
+    }
+    
+    private func configureKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        if contentTextView.isEditable {
+            self.moveWithKeyboard(self.contentTextViewBottomConstraint, on: notification, by: 30, up: true)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        self.moveWithKeyboard(self.contentTextViewBottomConstraint, on: notification, by: 30, up: false)
+    }
+    
+    private func moveWithKeyboard(_ constraint: NSLayoutConstraint, on notification: NSNotification, by value: CGFloat, up: Bool) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        guard let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        guard let curve = UIView.AnimationCurve(rawValue: userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as! Int) else { return }
+        
+        let keyboardHeight = keyboardSize.cgRectValue.height
+        
+        if up {
+            let safeArea = self.view.window?.safeAreaInsets.bottom != 0
+            constraint.constant = keyboardHeight + (safeArea ? 0 : value)
+        } else {
+            constraint.constant = value
+        }
+        
+        let animation = UIViewPropertyAnimator(duration: duration, curve: curve) { [weak self] in
+            self?.view?.layoutIfNeeded()
+        }
+        
+        animation.startAnimation()
     }
 }
